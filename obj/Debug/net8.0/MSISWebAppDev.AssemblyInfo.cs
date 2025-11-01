dded47cd3f0f62ff28e30f1050ab46174e7dfe61
Ray Bailey <ray.bailey11@okstate.edu>
2025-11-01 18:38:23 -0500

    Test commit

--- obj/Debug/net8.0/MSISWebAppDev.AssemblyInfo.cs
+++ obj/Debug/net8.0/MSISWebAppDev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSISWebAppDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e810b6c4bad2fb542f6406d816946b6bdcab33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f76afd45818bf34c6bf33e8b0bd626d679a29f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSISWebAppDev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSISWebAppDev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
